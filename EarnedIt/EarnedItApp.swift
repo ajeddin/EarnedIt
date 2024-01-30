476c360e673c9b32e4ad429f74d1ccb7aabc5981
Abdulaziz Jamaleddin <abduljeddin@gmail.com>
2024-01-30 08:21:31 -0500

    initial

---
+++ EarnedIt/EarnedItApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarnedItApp.swift
+//  EarnedIt
+//
+//  Created by Abdulaziz Jamaleddin on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct EarnedItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
