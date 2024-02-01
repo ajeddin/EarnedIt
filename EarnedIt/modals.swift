901e742716169ed1bf893eaa676ed88a69999bc9
Abdulaziz Jamaleddin <abduljeddin@gmail.com>
2024-02-01 00:01:39 -0500

    Amaozn

---
+++ EarnedIt/modals.swift
@@ -0,0 +1,15 @@
+//
+//  modals.swift
+//  EarnedIt
+//
+//  Created by Abdulaziz Jamaleddin on 1/31/24.
+//
+
+import Foundation
+struct Product{
+    var imageURL:String;
+    var productName:String;
+    var price:Float;
+    var rating:String;
+
+}
--- EarnedIt/wishList.swift
+++ EarnedIt/wishList.swift
@@ -12,7 +12,7 @@ struct wishList: View {
     var body: some View {
         ZStack{
             VStack{
-                WavePage(height1: 160 , height2: 190, isOn: false,duration1: 20,duration2: 25, showingText: true, headerText: "Wishlist",points: 30,isPresented:$presentedSheet)
+                WavePage(height1: 160 , height2: 190, isOn: true,duration1: 25,duration2: 30, showingText: true, headerText: "Wishlist",points: 30,isPresented:$presentedSheet)
             }}.sheet(isPresented: $presentedSheet, content: {
             wishListSheet()
         })
@@ -27,12 +27,104 @@ struct wishList: View {
     
 struct wishListSheet: View{
     @State var link: String = "";
+    @State var photo: String = "";
+    @State var itemName: String = "";
+    @State var price: String = "";
+    @State var amazonBool : Int = 0;
+    @State var response : String = "";
+    @State var isValidLink : Bool = false
+
     var body: some View{
-        
-        TextField("Hello", text: $link)
+        VStack {
+            Picker("What is your favorite color?", selection: $amazonBool) {
+                Text("Amazon Product").tag(1)
+                Text("Non-Amazon Product").tag(2)
+            }
+            .pickerStyle(.segmented)
+            
+            if (amazonBool==1){
+                Text("Please paste amazon link here:")
+                TextField("Link", text: $link)
+                Button(action: {
+                    if let range = link.range(of: "https") {
+                        var result = String(link[range.lowerBound...])
+                        result = result.trimmingCharacters(in: .whitespacesAndNewlines)
+                        link = result
+                        print(result)
+                    } else {
+                        print("Substring 'https' not found")
+                    }
+                    if var urlLink = URL(string: link), UIApplication.shared.canOpenURL(urlLink) {
+                    
+                    getRealImage(url: URL(string:link)!) { result in
+                        switch result {
+                        case .success(let response):
+//                            print("Image URL:  \(response)")
+                            getProductImage(url: URL(string:response)!) { result in
+                                switch result {
+                                case .success(let response):
+                                    photo = response.0
+                                    price = response.1
+                                    itemName = response.2
+                                    isValidLink = true
+
+                                case .failure(let error):
+                                    print("Error: \(error)")
+                                }
+                            }
+                            
+                        case .failure(let error):
+                            print("Error: \(error)")
+                        }
+                    }      
+                } else {
+                    Text("Invalid URL")
+                        .foregroundColor(.red)
+                }
+                    
+                }) {
+                Image(systemName: "plus")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 19, height: 19)
+                    .foregroundColor(Color("AccentColor"))
+                    .padding(15)
+                    .background(Color.white)
+                    .clipShape(Circle())
+            }
+                if (isValidLink){
+                    Text("\(price) \n \(itemName)\n")
+                    AsyncImage(url: URL(string: photo)) { phase in
+                                    if let image = phase.image {
+                                        image
+                                            .resizable()
+                                            .scaledToFit()
+                                    } else if phase.error != nil {
+                                        Text("There was an error loading the image.")
+                                    } else {
+                                        ProgressView()
+                                    }
+                                }
+                                .frame(width: 200, height: 200)
+                            }
+                
+            }
+            else if(amazonBool==2){
+                TextField("Link", text: $link)
+                TextField("Add Photo", text: $photo)
+                TextField("Item Name", text: $itemName)
+                TextField("Price", text: $price)
+            }
+            else{
+                Text("Choose Product Type to continue")
+                    .presentationDetents([.large])
+
+            }
+            Spacer()
+        }.padding(.top,80)
+      
 
 
-            .presentationDetents([.medium,.large])
     }
 }
         
