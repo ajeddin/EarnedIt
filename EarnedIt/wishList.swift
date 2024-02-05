5074c5ea46f940cb756069e104962b0ee1574066
Moe <mohamedmidani2001@gmail.com>
2024-02-05 11:20:01 -0500

    point counter update

--- EarnedIt/wishList.swift
+++ EarnedIt/wishList.swift
@@ -11,7 +11,7 @@ struct wishList: View {
     @Environment(\.modelContext) private var context
     @Query private var products: [Products];
     @State var presentedSheet : Bool = false;
-    @State var pointss : Int = 2;
+    @State private var points: Int = 0
 
     @Environment(\.accessibilityReduceMotion) var ReduceMotion;
 
@@ -20,7 +20,7 @@ struct wishList: View {
 
     var body: some View {
         ZStack{
-            WavePage(height1: 175 , height2: 210, isOn: !ReduceMotion,duration1: 28,duration2: 30, showingText: true, headerText: "Wishlist",points: pointss,isPresented:$presentedSheet)
+            WavePage(height1: 175 , height2: 210, isOn: !ReduceMotion,duration1: 28,duration2: 30, showingText: true, headerText: "Wishlist",points: $points,isPresented:$presentedSheet)
             
             VStack{
                 List{
@@ -68,7 +68,7 @@ struct wishList: View {
                             .swipeActions(edge: .leading) {
                                 Button(action: {
                                     //                                removeTask(at: task)
-                                    pointss = pointss + 1;
+                                   // pointss = pointss + 1;
                                 }) {
                                     Label("", systemImage: "gift")
                                 }
