03e01ac0f25ea7d9bb6f9cbfc24de5c9820bebb6
Abdulaziz Jamaleddin <abduljeddin@gmail.com>
2024-02-06 14:33:45 -0500

    AJ CODE

---
+++ redeemedView.swift
@@ -0,0 +1,18 @@
+//
+//  redeemedView.swift
+//  EarnedIt
+//
+//  Created by Abdulaziz Jamaleddin on 2/6/24.
+//
+
+import SwiftUI
+
+struct redeemedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    redeemedView()
+}
