f3974b24fa51bdc1c7ccd304787afd66df297864
Abdulaziz Jamaleddin <abduljeddin@gmail.com>
2024-02-11 21:38:34 -0500

    aj

--- EarnedIt/wishList.swift
+++ EarnedIt/wishList.swift
@@ -14,132 +14,136 @@ struct wishList: View {
     @State var userPoints : Int = 0
     @State var productPrice : Int = 0
     let haptic2 = UIImpactFeedbackGenerator(style: .heavy)
-
+    
     @State var presentedSheet : Bool = false;
-
-
+    
+    
     @Environment(\.accessibilityReduceMotion) var ReduceMotion;
-
-//    @State var arrayProducts: Product?;
-//    @AppStorage("arrayProducts")
-
+    
+    //    @State var arrayProducts: Product?;
+    //    @AppStorage("arrayProducts")
+    
     var body: some View {
         ZStack{
-            WavePage(buttonShwn:true,height1: 175 , height2: 210, isOn: !ReduceMotion,duration1: 28,duration2: 30, showingText: true, headerText: "Wishlist",isPresented:$presentedSheet)
-
+            WavePage(buttonShwn:true,height1: 160 , height2: 192, isOn: !ReduceMotion,duration1: 28,duration2: 30, showingText: true, headerText: "Wishlist",isPresented:$presentedSheet)
+            
             if products.filter({ $0.isRedeemed == false }).count == 0 {
-                if(!presentedSheet){
                     VStack(alignment: .center){
                         Image("noProducts").resizable().scaledToFit().frame(width: 100)
                         Text("Create your wishlist")
                         Text("Tap the plus button to get started")
-                    }
-                    
-                    
                 } }else{
                     VStack{
-
-                    List{
-                        Section{
-                            //                        ForEach(products.indices, id: \.self) { product in
-                            ForEach(products) { product in
-                                //                      if product.filter({ $0 }).isRedeemed = false {}
-                                if product.isRedeemed {}
-                                else{
-                                    HStack {
-                                        
-                                        AsyncImage(url: URL(string: product.imageURL)) { phase in
-                                            if let image = phase.image {
-                                                image
-                                                    .resizable()
-                                                    .scaledToFit()
-                                            } else if phase.error != nil {
-                                                Text("There was an error loading the image.")
-                                            } else {
-                                                ProgressView()
+                        
+                        List{
+                            Section{
+                                //                        ForEach(products.indices, id: \.self) { product in
+                                ForEach(products) { product in
+                                    //                      if product.filter({ $0 }).isRedeemed = false {}
+                                    if product.isRedeemed {}
+                                    else{
+                                        HStack {
+                                            if(product.imageURL == ""){
+                                                Image(systemName: "shippingbox").foregroundColor(Color("AccentColor"))
+                                                    .scaledToFill()
+                                                    .font(.largeTitle)
+                                                
+                                                    .frame(width: 80, height: 80)
+                                            }else{
+                                                
+                                                AsyncImage(url: URL(string: product.imageURL)) { phase in
+                                                    if let image = phase.image {
+                                                        image
+                                                            .resizable()
+                                                            .scaledToFit()
+                                                            .frame(width: 80, height: 80)
+                                                    } else if phase.error != nil {
+                                                        Text("There was an error loading the image.")
+                                                    } else {
+                                                        ProgressView()
+                                                    }
+                                                }
                                             }
-                                        }
-                                        .scaledToFit()
-                                        .frame(width: 80, height: 80)
-                                        Text(product.productName).padding(.leading,10).bold()
-                                    }
-                                    
-                                    .swipeActions {
-                                        Button(action:
-                                                {  context.delete(product)
-                                            try? context.save()
-                                            haptic2.impactOccurred(intensity: 1)
-                                        }) {
-                                            Label("", systemImage: "trash")
-                                        }
-                                        .tint(.red)
-                                        
-                                    }
-                                    //                            .swipeActions(edge: .leading) {
-                                    //                                Button(action: {
-                                    //                                    //                                removeTask(at: task)
-                                    //                                    defaults[0].points = defaults[0].points +  1;
-                                    //                                    try? context.save()
-                                    //                                }) {
-                                    //                                    Label("", systemImage: "gift")
-                                    //                                }
-                                    //                                .tint(.yellow)
-                                    //                            }
-                                    
-                                    HStack{
-                                        
-                                        Spacer()
-                                        if(defaults[0].points >= product.price){
                                             
-                                            Button{
-                                                defaults[0].points =   defaults[0].points - product.price
-                                                product.isRedeemed = true
+                                            Text(product.productName).padding(.leading,10).bold()
+                                        }
+                                        
+                                        .swipeActions {
+                                            Button(action:
+                                                    {  context.delete(product)
                                                 try? context.save()
+                                                haptic2.impactOccurred(intensity: 1)
+                                            }) {
+                                                Label("", systemImage: "trash")
                                             }
-                                        label: {
-                                            Text("Redeem").frame(alignment: .center).bold()
-                                        }
-                                        }
-                                        else{
-                                            Text("\(defaults[0].points)/\(product.price)").bold()
+                                            .tint(.red)
                                             
                                         }
-                                        Spacer()
+                                        //                            .swipeActions(edge: .leading) {
+                                        //                                Button(action: {
+                                        //                                    //                                removeTask(at: task)
+                                        //                                    defaults[0].points = defaults[0].points +  1;
+                                        //                                    try? context.save()
+                                        //                                }) {
+                                        //                                    Label("", systemImage: "gift")
+                                        //                                }
+                                        //                                .tint(.yellow)
+                                        //                            }
                                         
-                                    }
-                                }}
-                            //                                            .onDelete(perform: { indexSet in
-                            //                                                for index in indexSet{
-                            //                                                    context.delete(products[index])
-                            //                                                } })
-                            
-                        }header: {
-                            
-                            Text("Products")
-                        } }.scrollContentBackground(.hidden)
-                    
-                    
-                }.padding(.top,180).cornerRadius(25)
-            }
-
+                                        HStack{
+                                            
+                                            Spacer()
+                                            if(defaults[0].points >= product.price){
+                                                
+                                                Button{
+                                                    defaults[0].points =   defaults[0].points - product.price
+                                                    product.isRedeemed = true
+                                                    try? context.save()
+                                                }
+                                            label: {
+                                                Text("Redeem").frame(alignment: .center).bold()
+                                            }
+                                            }
+                                            else{
+                                                Text("\(defaults[0].points)/\(product.price)").bold()
+                                                
+                                            }
+                                            Spacer()
+                                            
+                                        }
+                                    }}
+                                //                                            .onDelete(perform: { indexSet in
+                                //                                                for index in indexSet{
+                                //                                                    context.delete(products[index])
+                                //                                                } })
+                                
+                            }header: {
+                                
+                                Text("Products")
+                            } }.scrollContentBackground(.hidden)
+                        
+                        
+                    }.padding(.top,180).cornerRadius(25)
+                }
+            
             
             
         }.sheet(isPresented: $presentedSheet, content: {
-                wishListSheet()  .presentationDetents([.medium])
+            wishListSheet().presentationDetents([.medium])
                 .presentationDragIndicator(.hidden)
-                
+            
         }
         )
         .animation(.bouncy)
         
-      
+        
         
         
         
         
     }
 }
-    
+
 
 
 
--- EarnedItShare/ShareViewController.swift
+++ EarnedItShare/ShareViewController.swift
@@ -49,31 +49,15 @@ fileprivate struct  SheetView: View {
 
     var body: some View{
         VStack{
-            if (link.hasPrefix("https://a.co") || link.hasPrefix("https://www.amazon.com")){
                 VStack{
                     HStack{
-                        Text(link)
+                        Text(link.prefix(1 + link.split(separator: "/").prefix(4).map { $0.count }.reduce(0, +)))
                     }
                     TextField("Enter Price", value: $price, formatter: Formatter.lucNumberFormat).keyboardType(.numberPad)
                     
                 }.padding(25)
 //                    .background(Color.white.opa city(0.7))
                 
-                
-                
-            }
-            else /*(!link.hasPrefix("https://a.co") || link.hasPrefix("https://www.amazon.com"))*/{
-                VStack{
-                    TextField("Enter Title", text: $title).autocorrectionDisabled()
-                        Text(link)
-                
-                    TextField("Enter Price", value: $price, formatter: Formatter.lucNumberFormat).keyboardType(.numberPad)
-                    
-                }.padding(25)
-//                    .background(Color.white.opacity(0.7))
-                
-                
-            }
             
             Button{
                 if (price <= 0){
@@ -92,7 +76,7 @@ fileprivate struct  SheetView: View {
                                     
                                     switch result {
                                     case .success(let response):
-                                        let product = Products(imageURL: response.0, productName: response.2.components(separatedBy: " ").prefix(5).joined(separator: " "), price: price, productLink: link)
+                                        let product = Products(imageURL: response.0, productName: response.2.components(separatedBy: " ").prefix(5).joined(separator: " "), price: price, productLink: response.1)
                                         saveItem(product: product)
                                         extentionContext!.completeRequest(returningItems: [])
                                         
@@ -109,9 +93,22 @@ fileprivate struct  SheetView: View {
                         
                     }
                     else{
-                        let product = Products(imageURL: "https://img.freepik.com/free-photo/cardboard-box_144627-20326.jpg?w=1480&t=st=1707146075~exp=1707146675~hmac=dd95c4723f0a52fee3a5b590d4e8c16b8c10fb4ced4cd19af3932bc0aa5510e5", productName: title, price: price, productLink: link)
-                        saveItem(product: product)
+                        Task {
+                            print(link)
+                            do {
+                                                let response = try await getProductTitleName(url: URL(string: link)!)
+                                                print(response.0)
+                                let product = Products(imageURL: "", productName: response.0.components(separatedBy: " ").prefix(5).joined(separator: " "), price: price, productLink: response.1)
+                                                context.insert(product)
+                                                try? context.save()
+                                
+                                // Handle the response as needed
+                                            } catch {
+                                                print("Error fetching data: \(error)")
+                                            }
+                                        }
                         extentionContext!.completeRequest(returningItems: [])
+
                     }
                 }
             
@@ -168,3 +165,176 @@ fileprivate struct  SheetView: View {
     }
 }
 
+
+
+
+////
+////  ShareViewController.swift
+////  EarnedItShare
+////
+////  Created by Abdulaziz Jamaleddin on 2/7/24.
+////
+//
+//import UIKit
+//import Social
+//import SwiftData
+//import SwiftUI
+//class ShareViewController: UIViewController {
+//    
+//    override func viewDidLoad() {
+//        
+//        super.viewDidLoad()
+//        isModalInPresentation = true
+//        if let items = (extensionContext!.inputItems.first as? NSExtensionItem)?.attachments{
+//            let hostingView = UIHostingController(rootView: SheetView(extentionContext: extensionContext,itemProvider: items).background(Color.clear).modelContainer(for: [Products.self,Tasks.self,UserChoices.self]))
+//            hostingView.view.frame = view.frame
+//            view.addSubview(hostingView.view)
+//            hostingView.view.backgroundColor = .clear
+//        }
+//  
+//        
+//        
+//    }
+//
+//}
+//extension Formatter {
+//    static let lucNumberFormat: NumberFormatter = {
+//        let formatter = NumberFormatter()
+//        formatter.numberStyle = .none
+//        formatter.zeroSymbol  = ""     // Show empty string instead of zero
+//        return formatter
+//    }()
+//}
+//fileprivate struct  SheetView: View {
+//    
+//    let haptic = UINotificationFeedbackGenerator()
+//
+//    @Environment(\.modelContext) private var context
+//    var extentionContext: NSExtensionContext?
+//    var itemProvider : [NSItemProvider]
+//    @State var price : Int = 0
+//    @State var link: String = ""
+//    @State var isAmazLink = false
+//    @State var title: String = ""
+//
+//    var body: some View{
+//        VStack{
+//            if (link.hasPrefix("https://a.co") || link.hasPrefix("https://www.amazon.com")){
+//                VStack{
+//                    HStack{
+//                        Text(link)
+//                    }
+//                    TextField("Enter Price", value: $price, formatter: Formatter.lucNumberFormat).keyboardType(.numberPad)
+//                    
+//                }.padding(25)
+////                    .background(Color.white.opa city(0.7))
+//                
+//                
+//                
+//            }
+//            else /*(!link.hasPrefix("https://a.co") || link.hasPrefix("https://www.amazon.com"))*/{
+//                VStack{
+//                    TextField("Enter Title", text: $title).autocorrectionDisabled()
+//                        Text(link)
+//                
+//                    TextField("Enter Price", value: $price, formatter: Formatter.lucNumberFormat).keyboardType(.numberPad)
+//                    
+//                }.padding(25)
+////                    .background(Color.white.opacity(0.7))
+//                
+//                
+//            }
+//            
+//            Button{
+//                if (price <= 0){
+//                                    haptic.notificationOccurred(.error)
+//                }else{
+//                                    haptic.notificationOccurred(.success)
+//
+//                    print(link)
+//                    
+//                    if (link.hasPrefix("https://a.co") || link.hasPrefix("https://www.amazon.com")){
+//                        getRealImage(url: URL(string:link)!) { result in
+//                            
+//                            switch result {
+//                            case .success(let response):
+//                                getProductImage(url: URL(string:response)!) { result in
+//                                    
+//                                    switch result {
+//                                    case .success(let response):
+//                                        let product = Products(imageURL: response.0, productName: response.2.components(separatedBy: " ").prefix(5).joined(separator: " "), price: price, productLink: link)
+//                                        saveItem(product: product)
+//                                        extentionContext!.completeRequest(returningItems: [])
+//                                        
+//                                        
+//                                    case .failure(let error):
+//                                        print("Error: \(error)")
+//                                    }
+//                                }
+//                                
+//                            case .failure(let error):
+//                                print("Error: \(error)")
+//                            }
+//                        }
+//                        
+//                    }
+//                    else{
+//                        let product = Products(imageURL: "", productName: title, price: price, productLink: link)
+//                        saveItem(product: product)
+//                        extentionContext!.completeRequest(returningItems: [])
+//                    }
+//                }
+//            
+//            } label: {Text("Done")}.buttonStyle(.borderedProminent)
+//
+//            
+//            
+//            
+//            
+//        }
+//            .onAppear(perform: {
+//            extract()
+//            
+//            
+//        })
+//            .background(Color("ForegroundColor"))
+//    }
+//
+//    
+//    func extract(){
+//        DispatchQueue.global(qos: .userInteractive).async{
+//            for provider in itemProvider {
+//                provider.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: { (attachment, error) in
+//                    if let url = attachment as? URL{
+//                        var urlText = url.absoluteString
+//                        if let range = urlText.range(of: "https") {
+//                            var result = String(urlText[range.lowerBound...])
+//                            result = result.trimmingCharacters(in: .whitespacesAndNewlines)
+//                            urlText = result
+//                           
+//
+//                        }
+//                                                DispatchQueue.main.async {
+//                        
+//                        link = urlText
+//                                                
+//                        print(link)
+//                    }
+//                }
+//                    
+//                })
+//            }
+//            
+//        }}
+//    func saveItem(product : Products){
+//        do{
+//            context.insert(product)
+//            try context.save()
+//        }catch{
+//            print(error.localizedDescription)
+//        }
+//        
+//        
+//    }
+//}
+//
